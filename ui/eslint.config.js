6546482ce7a26710d1dcdfd0b0724481ff21baf0
Maria Fernanda <maferzandona@gmail.com>
2026-06-10 01:11:36 -0300

    ci: ignore astro dist files on eslint config

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -1,12 +1,19 @@
 import js from '@eslint/js';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 export default [
+  { ignores: ["dist/", ".astro/"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      }
+    },
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
